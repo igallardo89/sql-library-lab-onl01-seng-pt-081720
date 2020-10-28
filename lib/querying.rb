ae57d3350680c1519848ba6ca6ace834a9baed9d
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 21:15:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
    FROM authors 
    INNER JOIN series ON series.author_id = authors.id
-   INNER JOIN subgenres"
+   INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
