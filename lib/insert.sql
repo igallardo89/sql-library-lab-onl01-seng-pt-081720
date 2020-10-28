ea6b3886207a21dda0538aed199093a6b4656799
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 20:42:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,12 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 3, 5),
 ("Jimmy Potter", 4, 10);
 
-INSERT INTO books (title, year, series_id) VALUES ("")
+INSERT INTO books (title, year, series_id) VALUES ("James Potter", 4, 9),
+("Lily Potter", 5, 19),
+("Ginny Potter", 7, 10),
+("Albus Potter", 8, 22),
+("Cho Potter", 9, 15),
+("Ron Potter", 10, 16);
+
 
 
