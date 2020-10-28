126d80d898d51f6d627cada079beadfd02664e85
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 21:35:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,9 +41,9 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
-  FROM character_books 
-  INNER JOIN characters 
-  ON character.id = character_books.character_id
-  GROUP BY character.name
-  ORDER BY COUNT(name) DESC;"
+    FROM character_books 
+    INNER JOIN characters 
+    ON character.id = character_books.character_id
+    GROUP BY characters.name
+    ORDER BY COUNT(name) DESC;"
 end
