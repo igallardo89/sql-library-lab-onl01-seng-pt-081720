15d337e1db6aff12144e4b94e3d9e73da1ef9cf9
Illiana Gallardo <igallardo0716@gmail.com>
2020-10-28 21:32:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,5 +40,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(name)
+  FROM character_books 
+  INNER JOIN character 
+  ON character.id = character_books.character_id
+  G"
 end
